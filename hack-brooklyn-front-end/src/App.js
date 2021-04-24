c67bf38521648aa951cce71e3fc43523b11bc96f
Daniel-Ho986 <dhonaruto@gmail.com>
2021-04-24 14:02:04 -0400

    Implemented the main page with dashboard

--- hack-brooklyn-front-end/src/App.js
+++ hack-brooklyn-front-end/src/App.js
@@ -1,13 +1,9 @@
-
+import Main from "./Components/Main/Main";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
+    <Main/>
   );
 }
 
