90e5ad175f97cdf85d2e64266815c1f88b53fe8f
Mohamed Shafee <mshafee1993@gmail.com>
2021-04-24 14:47:00 -0400

    minor fix

--- hack-brooklyn-front-end/src/Components/Main/Main.jsx
+++ hack-brooklyn-front-end/src/Components/Main/Main.jsx
@@ -1,18 +1,17 @@
-import React, {Component} from 'react';
-import {Carousel, Container} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Carousel, Container } from 'react-bootstrap';
 import Background from '../Background/Background';
-import Navbar from '../NavBar';
+import Navbar from '../Navbar';
 
-class Main extends Component{
-    render(){
-        return(
-            <div>
-                <Navbar/>
-            
-                <Background/>
-            </div>
-        )
-    }
+class Main extends Component {
+	render() {
+		return (
+			<div>
+				<Navbar />
 
+				<Background />
+			</div>
+		);
+	}
 }
 export default Main;
